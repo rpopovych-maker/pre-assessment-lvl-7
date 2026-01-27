18bad5b1848788c619bc9b2f3fc23370a901e30d
Roman Popovych <r.popovych@softonix.org>
2026-01-27 20:10:25 +0200

    Revert "test: test"
    This reverts commit 2b2324f1fdf9d3a6d4cef2f50ade56c68899a593.

--- src/components/TestModal.vue
+++ src/components/TestModal.vue
@@ -2,8 +2,8 @@
   <el-dialog
     :model-value="isOpen.TestModal"
     :before-close="() => closeModal('TestModal')"
-    class="no-header body-p-0 max-w-[700px]"
     destroy-on-close
+    class="no-header body-p-0 max-w-[700px]"
   >
     <div>Test Modal Content</div>
 
